11a3504b67566adb35cd3c22446e0ba9c1087943
skilletpro@gmail.com <>
2022-03-27 16:43:21 +0200

    Player can move but looking still need fixing currently player doesnt have a limit to camera rotation so can see world upside down.

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,20 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    PlayerInputActions playerInput;
     CharacterController characterController;
+    Camera playerView;
 
-    // Start is called before the first frame update
-    void Start()
+    Vector2 currentMovementInput;
+    Vector3 currentMovement;
+    Vector3 currentSprintMovement;
+
+    Vector2 currentLookInput;
+    Vector3 currentLook;
+    
+
+    float sprintModifier = 3f;
+    float movementSpeed = 5f;
+    bool isRunPressed;
+
+    private void Awake()
     {
-        characterController = gameObject.GetComponent<CharacterController>();
+        playerInput = new PlayerInputActions();
+        characterController = GetComponent<CharacterController>();
+        playerView = GetComponentInChildren<Camera>();
+
+        playerInput.Player.Move.started += onMovementInput;
+        playerInput.Player.Move.canceled += onMovementInput;
+        playerInput.Player.Move.performed += onMovementInput;
+        playerInput.Player.Sprint.started += onSprint;
+        playerInput.Player.Sprint.canceled += onSprint;
+        playerInput.Player.Sprint.performed += onSprint;
+        playerInput.Player.Look.started += onLookInput;
+        playerInput.Player.Look.canceled += onLookInput;
+        playerInput.Player.Look.performed += onLookInput;
+        
     }
+
+    void onLookInput(InputAction.CallbackContext context) {
+        currentLookInput = context.ReadValue<Vector2>();
+
+        playerView.transform.rotation = playerView.transform.rotation * Quaternion.Euler(currentLookInput.y, 0, 0);
+        transform.rotation = transform.rotation * Quaternion.Euler(0, currentLookInput.x, 0);
+    }
+
+    void onSprint(InputAction.CallbackContext context) {
+        isRunPressed = context.ReadValueAsButton();
+    }
+
+    void onMovementInput(InputAction.CallbackContext context) {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x * movementSpeed;
+        currentMovement.z = currentMovementInput.y * movementSpeed;
+        currentSprintMovement.x = currentMovementInput.x * movementSpeed * sprintModifier;
+        currentSprintMovement.z = currentMovementInput.y * movementSpeed * sprintModifier;
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        print(characterController.tag);
+        if (isRunPressed) {
+            characterController.Move(currentSprintMovement * Time.deltaTime);
+        } else {
+            characterController.Move(currentMovement * Time.deltaTime);
+        }
+
+    }
+
+    void OnEnable()
+    {
+        playerInput.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerInput.Player.Disable();
     }
 }
