d0a47e8c036c1f7822b6af6f7d9012cab8a528e9
skilletpro@gmail.com <>
2022-04-03 16:06:14 +0200

    Bottle now generates sound and can interact with drone. Currently drone doesnt react as this is a seperate feature.

--- Assets/Scripts/Drone/ScannerController.cs
+++ Assets/Scripts/Drone/ScannerController.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ScannerController : MonoBehaviour
 {
-
+    [SerializeField]
+    Transform scanOrigin;
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player") {
             RaycastHit hit;
-            Physics.Linecast(transform.position, other.transform.position, out hit);
+            Physics.Linecast(scanOrigin.position, other.transform.position, out hit);
             if (hit.collider.name == "Player") {
                 FindObjectOfType<GameManager>().LoadMainMenu();
             }
