e97fbe3f36027e4e83cfb4a74406044e12be7db1
skilletpro@gmail.com <>
2022-04-03 16:17:14 +0200

    Balancing changes to bottle reacction radius

--- Assets/Scripts/Misc/BottleController.cs
+++ Assets/Scripts/Misc/BottleController.cs
@@ -8,7 +8,7 @@ public class BottleController : MonoBehaviour
     Rigidbody rigibody;
     bool isThrown = false, expandingArea = false;
     private float expandingSpeed = 100f;
-    private float maxExpansionArea = 10f;
+    private float maxExpansionArea = 20f;
 
     private void Awake()
     {
